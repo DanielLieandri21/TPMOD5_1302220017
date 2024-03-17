1128e4e8337d507ad94f2b0f039e6127db777aee
DanielLieandri21 <dlieandri@gmail.com>
2024-03-18 04:58:27 +0700

    merge bench

--- TPMOD5_1302220017/Program.cs
+++ TPMOD5_1302220017/Program.cs
@@ -6,3 +6,27 @@
     }
 }
 
+public class DataGeneric<T>
+{
+    private T Data;
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+    }
+}
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser<String>("Daniel Lieandri");
+
+        DataGeneric<string> dataGeneric = new DataGeneric<string>("1302220017");
+        dataGeneric.PrintData();
+    }
+}
